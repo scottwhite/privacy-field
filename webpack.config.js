3494c82a221f12cbe5f371737153baf9ce87bff0
Scott White <scott@Scotts-MBP-2.lan>
2017-01-23 23:53:13 -0500

    add deploying to s3

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
 const path = require('path');
-const webpack = require('webpack'); 
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const S3Plugin = require('webpack-s3-plugin');
 
 module.exports = {
+  stats:{
+    errorDetails: true
+  },
   entry: './privacy-field.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -24,15 +29,30 @@ module.exports = {
         loader: 'html'
       }
     ]
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {warnings: true, unused: false}
-    }),
-    new CopyWebpackPlugin([{
-      from: 'privacy-field.js', to: 'privacy-field.js'
-    }])
-  ],
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {warnings: true, unused: false}
+      }),
+      new CopyWebpackPlugin([{
+        from: 'privacy-field.js', to: 'privacy-field.js'
+      }]),
+      new S3Plugin({
+        include: /.*\.js/,
+        s3Options: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+          region: process.env.AWS_REGION
+        },
+        s3UploadOptions: {
+          Bucket: process.env.AWS_BUCKET,
+          CacheControl(filename){
+            return 'no-store'
+          }
+        }
+      })
+    ]
+  ,
   //todo: get this working I guessssssssss
   // plugins: [
   //   new HtmlWebpackPlugin({
